92cf94128db9372805fd181f0922d9a331493936
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-08 16:30:52 +0100

    Slightly less transparent hovered button style

--- src/gui/colors.rs
+++ src/gui/colors.rs
@@ -17,11 +17,12 @@ pub const BUTTON_BG_SELECTED: Color = iced::Color {
     r: 255.,
     g: 255.,
     b: 255.,
-    a: 0.1,
+    a: 0.05,
 };
 
-pub const BUTTON_SECONDARY: Color = Color::from_rgb(
-    0x62 as f32 / 255.0,
-    0x69 as f32 / 255.0,
-    0x73 as f32 / 255.0,
-);
+pub const BUTTON_SECONDARY: Color = iced::Color {
+    r: 255.,
+    g: 255.,
+    b: 255.,
+    a: 0.07,
+};
