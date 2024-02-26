68d79f54204bd7c7518536dbc264895fbc90ea7b
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-26 18:23:41 +0100

    Use default config

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,16 @@ use core::commands::Command;
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short, long)]
-    json: String,
+    json: Option<String>,
 }
 
 fn main() -> iced::Result {
     let cli = Cli::parse();
 
-    let json_string = fs::read_to_string(cli.json).expect("Unable to read file");
+    let config_path = cli
+        .json
+        .unwrap_or_else(|| shellexpand::tilde("~/.config/iced_prompt/commands.json").into_owned());
+    let json_string = fs::read_to_string(config_path).expect("{} doesn't exist");
 
     let command: Command = serde_json::from_str(&json_string).expect("Unable to parse json");
 
